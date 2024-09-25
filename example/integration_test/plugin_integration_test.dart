0e2ec14fed5b1b1ac119e6ee335c2465562fe0c1
Nathan Friedly <nathan@fullstory.com>
2024-09-25 11:48:12 -0400

    rename fullstory.dart -> fs.dart to match the new class name

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -9,7 +9,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:fullstory_flutter/fullstory.dart';
+import 'package:fullstory_flutter/fs.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
