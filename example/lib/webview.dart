0c01fa63019f74bdd0f4ac577e6edd3cf47800cb
Nathan Friedly <nathan@fullstory.com>
2024-10-29 12:00:16 -0400

    add a webview to the testing app

---
+++ example/lib/webview.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebView extends StatefulWidget {
+  const WebView({super.key});
+
+  @override
+  State<WebView> createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://www.fullstory.com/'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebViewWidget(controller: controller);
+  }
+}
