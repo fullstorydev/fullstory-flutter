e47e5061f325b6de333e508089de7519696cbccc
Nathan Friedly <nathan@fullstory.com>
2024-11-04 16:22:57 -0500

    add a fullstory_flutter.dart file so that pub.dev knows which file to recommend importing

---
+++ lib/fullstory_flutter.dart
@@ -0,0 +1,3 @@
+// This file exists for pub.dev, because otherwise it doesn't know which file to instruct users to import at https://pub.dev/packages/fullstory_flutter/install
+// It's functionally equivalent to importing fs.dart directly.
+export 'fs.dart';
