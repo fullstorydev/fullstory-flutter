aedcf9cd0dfeeb052c142a41069c2c216f6e4a98
Conner Kasten <connerkasten@fullstory.com>
2025-01-14 10:43:40 -0500

    Fix formatting

--- lib/fs_log_level.dart
+++ lib/fs_log_level.dart
@@ -1,12 +1,12 @@
 /// Log levels. Controls how logs appear in the Dev tools > Console section of a Fullstory session replay
-/// 
+///
 /// These correspond to the levels shown in session replay,
 /// not the the levels used in Android/iOS logging.
 enum FSLogLevel {
   // Note: the order matters; the index is used to match to the appropriate value in the android and ios platform code
 
   /// Lower than default level logging.
-  /// 
+  ///
   /// Equivalent to debug level on Android and iOS.
   log,
 
@@ -16,8 +16,8 @@ enum FSLogLevel {
   /// Warn log level
   warn,
 
-  /// Error log level. 
-  /// 
+  /// Error log level.
+  ///
   /// Will appear in in both the console and the event list in Fullstory
   error,
 
