ec3628756cb8061c061e780f7078c088833df22b
Conner Kasten <connerkasten@fullstory.com>
2025-05-16 15:27:07 -0400

    cleanup

--- example/lib/network_events.dart
+++ example/lib/network_events.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fullstory_dio/fullstory_dio.dart';
 
@@ -34,21 +31,4 @@ class NetworkEvents extends StatelessWidget {
 }
 
 // This is declared at the top-level to allow [NetworkEvents] to be const.
-Dio get dio => Dio()
-  ..interceptors.add(FullstoryInterceptor(
-    computeRequestSize: (rq) => _utf8BytesOf(rq.data),
-    computeResponseSize: (rs) => _utf8BytesOf(rs?.data),
-  ));
-
-int _utf8BytesOf(dynamic data) {
-  print('data type: ${data.runtimeType}');
-  if(data == null) return 0;
-  if(data is Uint8List) data.length;
-
-  final str = data.toString();
-  if (str.isEmpty) return 0;
-
-  return _utf8.encode(str).length;
-}
-
-const _utf8 = Utf8Codec();
+Dio get dio => Dio()..interceptors.add(FullstoryInterceptor());
