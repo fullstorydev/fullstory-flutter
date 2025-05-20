b405f3427291c4baa226740451735fdca261d522
Conner Kasten <connerkasten@fullstory.com>
2025-05-20 10:19:44 -0400

    Add interceptor for package:http with test, example, etc.

--- example/lib/network_events.dart
+++ example/lib/network_events.dart
@@ -3,10 +3,15 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fullstory_flutter/fs.dart';
+import 'package:fullstory_flutter/network/dio.dart' as fs_dio;
+import 'package:fullstory_flutter/network/http.dart' as fs_http;
+import 'package:http/http.dart';
 import 'package:fullstory_flutter/dio.dart';
 
 // This is declared at the top-level to allow [NetworkEvents] to be const.
-Dio get dio => Dio()..interceptors.add(FSInterceptor());
+Dio get dio => Dio()..interceptors.add(fs_dio.FSInterceptor());
+Client get httpClient => fs_http.fsHttpClient();
 
 /// Demo widget that sends network requests using package:dio and package:http.
 class NetworkEvents extends StatelessWidget {
@@ -17,11 +22,27 @@ class NetworkEvents extends StatelessWidget {
     return Column(
       children: [
         TextButton(
+          onPressed: () => _makeHttpRequest(context),
+          child: const Text('Send package:http request'),
+        ),
+        TextButton(
           onPressed: () => _makeDioRequest(context),
           child: const Text('Send dio request'),
         ),
       ],
     );
+  }
+
+  Future<void> _makeHttpRequest(BuildContext context) async {
+    print(FS.currentSessionURL());
+    final response = await httpClient.get(Uri.parse('https://fullstory.com'));
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('package:http got response: ${response.statusCode}'),
+        ),
+      );
+    }
   }
 
   Future<void> _makeDioRequest(BuildContext context) async {
@@ -35,3 +56,4 @@ class NetworkEvents extends StatelessWidget {
     }
   }
 }
+
---
+++ lib/network/http.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:fullstory_flutter/fs.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+/// Captures requests and responses from package:http for display in Fullstory.
+///
+/// Either include this interceptor in your own [InterceptedClient] or call
+/// [fsHttpClient] to create a new [Client] with this interceptor preconfigured.
+class FSInterceptor extends InterceptorContract {
+  // Map of request URLs to epoch start times in milliseconds.
+  final _requestStartTimes = <Uri, int>{};
+
+  @override
+  FutureOr<BaseRequest> interceptRequest({required BaseRequest request}) {
+    _requestStartTimes[request.url] = DateTime.now().millisecondsSinceEpoch;
+    return request;
+  }
+
+  @override
+  FutureOr<BaseResponse> interceptResponse({required BaseResponse response}) {
+    FS.networkEvent(
+      url: response.request?.url.toString() ?? '',
+      statusCode: response.statusCode,
+      method: response.request?.method ?? '',
+      requestSize: response.request?.contentLength,
+      responseSize: response.contentLength,
+      durationMs: _durationOf(response.request?.url),
+    );
+
+    return response;
+  }
+
+  int _durationOf(Uri? uri) {
+    if (uri == null) return 0;
+
+    final startTime = _requestStartTimes.remove(uri);
+    return startTime == null
+        ? 0
+        : DateTime.now().millisecondsSinceEpoch - startTime;
+  }
+}
+
+/// Creates an HTTP [Client] which intercepts and captures network events for
+/// Fullstory.
+///
+/// Whether Fullstory captures events before or after other interceptors is
+/// specified by [captureFirst]. This may be important if you have interceptors
+/// which modify the request or response. In this case, you can configure
+/// whether these modifications are captured by Fullstory or not.
+Client fsHttpClient({
+  List<InterceptorContract> interceptors = const [],
+  Duration? requestTimeout,
+  TimeoutCallback? onRequestTimeout,
+  RetryPolicy? retryPolicy,
+  Client? client,
+  bool captureFirst = true,
+}) =>
+    InterceptedClient.build(
+      interceptors: [
+        if (captureFirst) FSInterceptor(),
+        ...interceptors,
+        if (!captureFirst) FSInterceptor(),
+      ],
+      requestTimeout: requestTimeout,
+      onRequestTimeout: onRequestTimeout,
+      retryPolicy: retryPolicy,
+      client: client,
+    );
