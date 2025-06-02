e54021073e79fbf5898a10f7bd178d13afbbb9f8
Conner Kasten <connerkasten@fullstory.com>
2025-06-02 14:10:51 -0400

    remove unnecessary default values.

--- lib/fs.dart
+++ lib/fs.dart
@@ -73,10 +73,10 @@ class FS {
   static Future<void> networkEvent({
     required String url,
     required String method,
-    int? statusCode = 0,
-    int? durationMs = 0,
-    int? requestSize = 0,
-    int? responseSize = 0,
+    int? statusCode,
+    int? durationMs,
+    int? requestSize,
+    int? responseSize,
   }) =>
       FullstoryFlutterPlatform.instance.captureEvent(<String, Object?>{
         // This event type is assigned to network events in host SDKs
