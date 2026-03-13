82ff80ad9e07c7ec3bdb0bc4037e0eaf279db086
Conner Kasten <connerkasten@fullstory.com>
2026-03-13 12:52:35 -0400

    Fix iOS tests

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -8,20 +8,30 @@ import XCTest
 // This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
 //
 // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
-
+// Run with:
+// xcodebuild test -workspace Runner.xcworkspace -scheme Runner -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 16,OS=latest'
 class RunnerTests: XCTestCase {
 
-  func testGetPlatformVersion() {
-    let plugin = FullstoryFlutterPlugin()
-
-    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
-
+  func testGetFsVersion() {
+    // This mock channel is only used to for the status listener API
+    let channel = MockMethodChannel()
+    let plugin = FullstoryFlutterPlugin(channel: channel)
+      
+    let call = FlutterMethodCall(methodName: "fsVersion", arguments: [])
     let resultExpectation = expectation(description: "result block must be called.")
     plugin.handle(call) { result in
-      XCTAssertEqual(result as! String, "iOS " + UIDevice.current.systemVersion)
+      // This value comes from the underlying iOS FS implementation
+      XCTAssertEqual(result as! String, "1.68.2")
       resultExpectation.fulfill()
     }
     waitForExpectations(timeout: 1)
   }
+}
 
+class MockMethodChannel: FlutterMethodChannel {
+  var didCallMethod: String?
+
+  override func invokeMethod(_ method: String, arguments: Any?, result: FlutterResult?) {
+    didCallMethod = method
+  }
 }
